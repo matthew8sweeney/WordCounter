966365360e6d19f0e898f3d00bf0a5ea493df6ff
Matthew Sweeney <msweeney@iwu.edu>
2020-10-14 21:17:35 -0500

    Switch to RichTextBox editor, load and save Rtf or XamlPackage

--- WordCounter/MainWindow.xaml.cs
+++ WordCounter/MainWindow.xaml.cs
@@ -3,6 +3,8 @@ using System;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
+
 
 namespace WordCounter
 {
@@ -11,6 +13,8 @@ namespace WordCounter
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string XamlExtension = ".xaml";
+
         public MainWindow()
         {
             InitializeComponent();
@@ -18,11 +22,16 @@ namespace WordCounter
 
         private void StatDisplayUpdater(object sender, TextChangedEventArgs e)
         {
-            int wordCount = CountWords(TextEntry.Text);
-            int charCount = TextEntry.Text.Length;
+            // aquire the text from the RichTextBox
+            string text = new TextRange(textEntry.Document.ContentStart, textEntry.Document.ContentEnd).Text;
 
-            WordCountDisplay.Text = wordCount.ToString();
-            CharCountDisplay.Text = charCount.ToString();
+            // calculate word and char counts
+            int wordCount = CountWords(text);
+            int charCount = text.Length;
+
+            // update the word and char count displays
+            wordCountDisplay.Text = wordCount.ToString();
+            charCountDisplay.Text = charCount.ToString();
 
             //DebugInfoDisplay.Text = "e:" + e.OriginalSource;
             //DebugInfoDisplay.Text = "sender Type:" + sender.GetType().ToString();
@@ -58,15 +67,89 @@ namespace WordCounter
             return count;
         }
 
+        /// <summary>
+        /// Save the content of the RichTextBox to a file
+        /// </summary>
         private void SaveText(object sender, RoutedEventArgs e)
         {
-            SaveFileDialog sfd = new SaveFileDialog();
+            string format;
+            SaveFileDialog sfd = new SaveFileDialog
+            {
+                Filter = "Xaml Package Files (*.xaml)|*.xaml"
+            };
+
+            // show file dialog, then if a filename is chosen...
             if (sfd.ShowDialog() == true)
             {
-                File.WriteAllText(sfd.FileName, TextEntry.Text);
+                TextRange range = new TextRange(
+                    textEntry.Document.ContentStart,
+                    textEntry.Document.ContentEnd
+                );
+
+                // detect file format
+                if (Path.GetExtension(sfd.FileName) == XamlExtension)
+                {
+                    format = DataFormats.XamlPackage;
+                    debugInfoDisplay.Text = "Saved to " + DataFormats.XamlPackage;
+                }
+                else
+                {
+                    format = DataFormats.Rtf;
+                    debugInfoDisplay.Text = "Saved to " + DataFormats.Rtf;
+                }
+
+                // save the content to a file
+                FileStream fs = new FileStream(sfd.FileName, FileMode.Create);
+                range.Save(fs, format);
+                fs.Close();
             }
         }
 
+        /// <summary>
+        /// Load content into the RichTextBox editor from a file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void LoadText(object sender, RoutedEventArgs e)
+        {
+            TextRange range;
+            FileStream fs;
+            string format;
+            OpenFileDialog ofd = new OpenFileDialog
+            {
+                Filter = "Xaml Package Files (*.xaml)|*.xaml|Rich Text Format Files (*.rtf)|*.rtf"
+            };
+
+            // show file dialog, then if a file is chosen...
+            if (ofd.ShowDialog() == true)
+            {
+                range = new TextRange(
+                    textEntry.Document.ContentStart,
+                    textEntry.Document.ContentEnd
+                );
+
+                // detect file format
+                if (Path.GetExtension(ofd.FileName) == XamlExtension)
+                {
+                    format = DataFormats.XamlPackage;
+                    debugInfoDisplay.Text = "Loaded " + DataFormats.XamlPackage;
+                }
+                else
+                {
+                    format = DataFormats.Rtf;
+                    debugInfoDisplay.Text = "Loaded" + DataFormats.Rtf;
+                }
+
+                // load from the file
+                fs = new FileStream(ofd.FileName, FileMode.OpenOrCreate);
+                range.Load(fs, format);
+                fs.Close();
+            }
+        }
+
+        /// <summary>
+        /// Exit the application gracefully
+        /// </summary>
         private void ExitApp(object sender, RoutedEventArgs e)
         {
             System.Windows.Application.Current.Shutdown();
